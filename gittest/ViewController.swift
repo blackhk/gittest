ef172625193e25bf4ca960976870c7a18e0e3e1f
Artem Pomelnikov <artpomdev@gmail.com>
2015-07-05 16:33:34 +0300

    ff

---
+++ gittest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Artem Pomelnikov on 05.07.15.
+//  Copyright (c) 2015 Artem Pomelnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var d= ""
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gittestTests/gittestTests.swift
@@ -0,0 +1,36 @@
+//
+//  gittestTests.swift
+//  gittestTests
+//
+//  Created by Artem Pomelnikov on 05.07.15.
+//  Copyright (c) 2015 Artem Pomelnikov. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class gittestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

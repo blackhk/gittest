97dcf5b6d6f7d6a2d246d258762c21de5325b0c8
Artem Pomelnikov <artpomdev@gmail.com>
2015-07-05 16:37:20 +0300

    2

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var d = ""
+        var d = "2"
         
         // Do any additional setup after loading the view, typically from a nib.
     }

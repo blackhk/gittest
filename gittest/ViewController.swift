e764cb1344f31568750ef3a1c90979a624f93c86
Artem Pomelnikov <artpomdev@gmail.com>
2015-07-05 17:32:08 +0300

    7

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var d = "6"
+        var d = "7"
         
         // Do any additional setup after loading the view, typically from a nib.
     }
